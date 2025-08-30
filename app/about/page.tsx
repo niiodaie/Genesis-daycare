ab73429faeac5397f15b69bb6ba88e66675b9f83
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-08-30 05:27:29 -0500

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -16,7 +16,7 @@ export default function AboutPage() {
         <div>
           <span className="badge">About Us</span>
           <h1 className="mt-2 text-3xl md:text-4xl font-extrabold heading">
-            About <span className="text-royalRed">GENESIS</span>{" "}
+            <span className="text-royalRed">GENESIS</span>{" "}
             <span className="text-royalYellow">Royalty</span>
           </h1>
           <p className="mt-3 max-w-2xl text-slate-700">
